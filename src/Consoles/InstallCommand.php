7bd1bf5e052cec6d78bbd87c8c7348ef4acb9365
Tanwen <tanwen@Tanwen.local>
2018-06-26 15:06:18 +0800

    update

--- src/Consoles/InstallCommand.php
+++ src/Consoles/InstallCommand.php
@@ -4,6 +4,8 @@ namespace Tanwencn\Blog\Consoles;
 
 use Encore\Admin\Admin;
 use Illuminate\Console\Command;
+use Spatie\Permission\PermissionServiceProvider;
+use Tanwencn\Blog\BlogServiceProvider;
 use Tanwencn\Blog\CmsServiceProvider;
 
 class InstallCommand extends Command
@@ -42,8 +44,29 @@ class InstallCommand extends Command
         $this->call('key:generate');
         $this->call('storage:link');
 
-        $this->call('vendor:publish');
-        $this->call('migrate:install');
+        $this->call('vendor:publish', [
+            '--provider' => BlogServiceProvider::class
+        ]);
+        $this->call('vendor:publish', [
+            '--provider' => PermissionServiceProvider::class,
+            '--tag' => 'migrations'
+        ]);
+        $this->call('migrate');
+
+        $this->info('Attempting to set User model as parent to App\User');
+        if (file_exists(app_path('User.php'))) {
+            $str = file_get_contents(app_path('User.php'));
+
+            if ($str !== false) {
+                $str = str_replace('extends Authenticatable', "extends \Tanwencn\Blog\Database\Eloquent\User", $str);
+
+                file_put_contents(app_path('User.php'), $str);
+            }
+        } else {
+            $this->warn('Unable to locate "app/User.php".  Did you move this file?');
+            $this->warn('You will need to update this manually.  Change "extends Authenticatable" to "extends \Tanwencn\Blog\Database\Eloquent\User" in your User model');
+        }
+
         $this->call('blog:registerPermissions');
     }
 }
